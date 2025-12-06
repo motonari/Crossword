a6be5a187a94b946128d86c373deadfb6708df50
Moto ITO <motonari.ito@gmail.com>
2025-12-06 07:50:39 -0800

    Accept multiple mandatory words.

--- Sources/CrosswordMaker/CrosswordMaker.swift
+++ Sources/CrosswordMaker/CrosswordMaker.swift
@@ -7,8 +7,12 @@ struct CrosswordMaker: ParsableCommand {
     @Flag(name: .short, help: "Generate layout file.")
     var generateLayoutFile = false
 
-    @Option(name: .shortAndLong, help: "A word to include in the puzzle.")
-    var mustWord: String?
+    @Option(
+        name: [.customShort("m"), .customLong("mandatory-words")],
+        help: "List of words the puzzle must have.",
+        transform: { Word($0.uppercased()) }
+    )
+    var mandatoryWords: [Word]
 
     @Option(name: .shortAndLong, help: "The output file.")
     var outputFileName: String
@@ -54,90 +58,6 @@ struct CrosswordMaker: ParsableCommand {
         }
     }
 
-    func makeCrosswordTest(
-        grid: Grid,
-        wordCount: Int,
-        lexicon: [Word],
-        mustWords: [Word]
-    ) {
-        let layout = Layout(
-            grid: grid,
-            blackCells: [
-                (4, 0),
-                (5, 0),
-
-                (1, 1),
-                (3, 1),
-                (5, 1),
-                (7, 1),
-                (8, 1),
-                (10, 1),
-
-                (1, 2),
-                (7, 2),
-
-                (0, 3),
-                (1, 3),
-                (2, 3),
-                (3, 3),
-                (5, 3),
-                (6, 3),
-                (7, 3),
-                (8, 3),
-                (10, 3),
-
-                (0, 4),
-                (1, 4),
-                (3, 4),
-                (5, 4),
-                (7, 4),
-
-                (0, 5),
-                (7, 5),
-                (8, 5),
-                (10, 5),
-
-                (1, 6),
-                (3, 6),
-                (5, 6),
-                (7, 6),
-                (11, 6),
-
-                (8, 7),
-                (9, 7),
-                (10, 7),
-                (11, 7),
-
-                (1, 8),
-                (3, 8),
-                (5, 8),
-                (7, 8),
-                (8, 8),
-                (10, 8),
-
-                (0, 9),
-                (10, 9),
-
-                (0, 10),
-                (1, 10),
-                (3, 10),
-                (5, 10),
-                (6, 10),
-                (7, 10),
-                (8, 10),
-                (10, 10),
-
-                (8, 11),
-            ])
-
-        let grid = Grid(width: width, height: height)
-        let crossword = Crossword(grid: grid, with: layout)
-
-        let solver = Solver(for: crossword, lexicon: lexicon, mustWords: mustWords)
-
-        _ = solver.solve()
-    }
-
     mutating func run() throws {
         let grid = Grid(width: width, height: height)
 
@@ -148,18 +68,11 @@ struct CrosswordMaker: ParsableCommand {
         }
 
         let lexicon = Lexicon().words
-        let mustWords: [Word] =
-            if let mustWord {
-                [Word(mustWord.uppercased())]
-            } else {
-                []
-            }
-
-        print("Solving with word count = \(count), mustWord: \(mustWords)")
+        print("Solving with word count = \(count), mandatory words: \(mandatoryWords)")
         try makeCrossword(
             grid: grid,
             wordCount: count,
             lexicon: lexicon,
-            mustWords: mustWords)
+            mustWords: mandatoryWords)
     }
 }
