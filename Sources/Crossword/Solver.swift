8e67f312fdc6a073049ca12ab41a195a2768c3f2
Moto ITO <motonari.ito@gmail.com>
2025-12-07 18:32:06 -0800

    Clean up.

--- Sources/Crossword/Solver.swift
+++ Sources/Crossword/Solver.swift
@@ -25,11 +25,27 @@ extension Solver {
 
 /// Arc-Consistency
 extension Solver {
+    /// Reduce all the domains per arc-consistency.
+    ///
+    /// - Parameters:
+    ///  - solution: The solution to update.
+    ///
+    /// - Returns:
+    ///  True if the solution is solvable, otherwise false.
     func enforceArcConsistency(solution: inout Solution) -> Bool {
         var workQueue = Deque(crossword.overlaps.keys)
         return enforceArcConsistencyInternal(solution: &solution, workQueue: &workQueue)
     }
 
+    /// Reduce domains of spans that depend on a specified span.
+    ///
+    /// - Parameters:
+    ///  - solution: The solution to update.
+    ///  - span: The span which was updated. Any span that intersects
+    ///  with this span will be updated according to arc-consistency.
+    ///
+    /// - Returns:
+    ///  True if the solution is solvable, otherwise false.
     func enforceArcConsistency(solution: inout Solution, span: Span) -> Bool {
         let workItems = crossword.spansIntersecting(with: span).map { SpanPair($0, span) }
         var workQueue = Deque(workItems)
@@ -40,6 +56,7 @@ extension Solver {
         solution: inout Solution,
         workQueue: inout Deque<SpanPair>
     ) -> Bool {
+        // AC-3 algorithm.
         var success = true
         while let spanPair = workQueue.popFirst() {
             let targetSpan = spanPair.span1
@@ -64,6 +81,49 @@ extension Solver {
 
 /// Global-Consistency
 extension Solver {
+    /// Enforce the rule that Crossword puzzle cannot use a same word
+    /// multiple times.
+    ///
+    /// This function made the whole solution globally consistent.
+    func enforceGlobalConsistency(solution: inout Solution) -> Bool {
+        var workQueue = Deque<SpanPair>()
+
+        for referenceSpan in crossword.spans {
+            let newWorkItems = globalConsistencyWorkItems(of: solution, for: referenceSpan)
+            workQueue.append(contentsOf: newWorkItems)
+        }
+
+        return enforceGlobalConsistencyInternal(solution: &solution, workQueue: &workQueue)
+    }
+    /// Enforce the rule: Crossword puzzle cannot use a same word
+    /// multiple times.
+    ///
+    /// This function made dependent spans globally consistent, given
+    /// the span that has been assigned a single value.
+    func enforceGlobalConsistency(solution: inout Solution, span referenceSpan: Span) -> Bool {
+        let newWorkItems = globalConsistencyWorkItems(of: solution, for: referenceSpan)
+        var workQueue = Deque<SpanPair>(newWorkItems)
+        return enforceGlobalConsistencyInternal(solution: &solution, workQueue: &workQueue)
+    }
+
+    private func globalConsistencyWorkItems(of solution: Solution, for referenceSpan: Span)
+        -> [SpanPair]
+    {
+        var workItems = [SpanPair]()
+
+        let referenceDomain = solution.domain(for: referenceSpan)
+        guard referenceDomain.count == 1 else {
+            return []
+        }
+
+        for targetSpan in crossword.spans {
+            guard referenceSpan != targetSpan else { continue }
+            guard solution.domain(for: targetSpan).count > 1 else { continue }
+            workItems.append(SpanPair(targetSpan, referenceSpan))
+        }
+        return workItems
+    }
+
     private func enforceGlobalConsistencyInternal(
         solution: inout Solution,
         workQueue: inout Deque<SpanPair>
@@ -91,44 +151,6 @@ extension Solver {
             }
         }
         return success
-    }
-
-    /// Enforce the rule: Crossword puzzle cannot use a same word
-    /// multiple times.
-    func enforceGlobalConsistency(solution: inout Solution) -> Bool {
-        var workQueue = Deque<SpanPair>()
-
-        for referenceSpan in crossword.spans {
-            let newWorkItems = globalConsistencyWorkItems(of: solution, for: referenceSpan)
-            workQueue.append(contentsOf: newWorkItems)
-        }
-
-        return enforceGlobalConsistencyInternal(solution: &solution, workQueue: &workQueue)
-    }
-    /// Enforce the rule: Crossword puzzle cannot use a same word
-    /// multiple times.
-    func enforceGlobalConsistency(solution: inout Solution, span referenceSpan: Span) -> Bool {
-        let newWorkItems = globalConsistencyWorkItems(of: solution, for: referenceSpan)
-        var workQueue = Deque<SpanPair>(newWorkItems)
-        return enforceGlobalConsistencyInternal(solution: &solution, workQueue: &workQueue)
-    }
-
-    private func globalConsistencyWorkItems(of solution: Solution, for referenceSpan: Span)
-        -> [SpanPair]
-    {
-        var workItems = [SpanPair]()
-
-        let referenceDomain = solution.domain(for: referenceSpan)
-        guard referenceDomain.count == 1 else {
-            return []
-        }
-
-        for targetSpan in crossword.spans {
-            guard referenceSpan != targetSpan else { continue }
-            guard solution.domain(for: targetSpan).count > 1 else { continue }
-            workItems.append(SpanPair(targetSpan, referenceSpan))
-        }
-        return workItems
     }
 }
 
